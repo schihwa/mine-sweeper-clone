6667d547881f5e41cd1188c906b751e8ad2a0c47
Seth <77753297+schihwa@users.noreply.github.com>
2023-11-10 05:40:26 +0000

    consecutive mine count

--- Resources/BombSquare.java
+++ Resources/BombSquare.java
@@ -13,11 +13,11 @@ public class BombSquare extends GameSquare {
 	private final boolean hasBomb;
 	private boolean isRevealed = false;
 
-	private static final int[][] directionOffsets = { // Direction offsets array
-			{ -1, -1 }, { -1, 0 }, { -1, 1 },
-			{ 0, -1 }, 			   { 0, 1 },
-			{ 1, -1 }, { 1, 0 },   { 1, 1 }
-	};
+    private static final int[][] directionOffsets = { // Direction offsets array
+        {-1, -1}, {-1, 0}, {-1, 1},
+        {0, -1},           {0, 1},
+        {1, -1},  {1, 0},  {1, 1}
+    };
 
 	/**
 	 * Constructor for BombSquare that initializes the square with an image and
@@ -65,23 +65,26 @@ public class BombSquare extends GameSquare {
 	}
 
 	/**
-	 * Counts the number of consecutive bombs in a given direction from this square.
+	 * Counts the number of consecutive bombs in a given direction from this square,
+	 * up to a maximum of 8 steps.
 	 *
 	 * @param deltaX The x-direction offset.
 	 * @param deltaY The y-direction offset.
 	 * @return The count of consecutive bombs in the direction until a non-bomb
-	 *         square is found.
+	 *         square is found or the maximum of 8 steps is reached.
 	 */
 	private int countBombsInDirection(int deltaX, int deltaY) {
 		int bombCount = 0;
+		int steps = 0; // Counter for the number of steps taken in the direction
 		int checkX = xLocation + deltaX;
 		int checkY = yLocation + deltaY;
 
-		// Keep checking in this direction until we find a non-bomb or go out of bounds.
-		while (true) {
+		// Keep checking in this direction until we find a non-bomb, go out of bounds,
+		// or reach 8 steps.
+		while (steps < 8) {
 			GameSquare square = board.getSquareAt(checkX, checkY);
 
-			// Stop checking if out of bounds or not a BombSquare, or if it doesn't have a
+			// Stop checking if out of bounds, not a BombSquare, or if it doesn't have a
 			// bomb.
 			if (!(square instanceof BombSquare) || !((BombSquare) square).hasBomb) {
 				break;
@@ -91,6 +94,7 @@ public class BombSquare extends GameSquare {
 			bombCount++;
 			checkX += deltaX;
 			checkY += deltaY;
+			steps++; // Increment the steps counter
 		}
 
 		return bombCount;
